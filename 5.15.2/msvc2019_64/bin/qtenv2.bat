df845c6212f8d40d7fa0a161da6b989b31291931
Anna L. Sheaffer <asheaf01@eecs.tufts.edu>
2025-03-01 14:52:32 -0500

    LSL environment setup

---
+++ 5.15.2/msvc2019_64/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=/mnt/c/Users/annsb/Documents/HCI-Lab/cabcs_lsl/5.15.2/msvc2019_64/bin;%PATH%
+cd /D /mnt/c/Users/annsb/Documents/HCI-Lab/cabcs_lsl/5.15.2/msvc2019_64
+echo Remember to call vcvarsall.bat to complete environment setup!
